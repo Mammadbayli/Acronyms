62a833ef1162e4573df84a4b2763449b1e3f5ef9
Javad <javad@mammadbayli.com>
2022-11-09 15:22:04 -0800

    add throttling to search bar input

--- Acronyms/ViewModel/ImplementsSearchAcronymsViewControllerViewModel.swift
+++ Acronyms/ViewModel/ImplementsSearchAcronymsViewControllerViewModel.swift
@@ -8,11 +8,24 @@
 import Foundation
 class ImplementsSearchAcronymsViewControllerViewModel: SearchAcronymsViewControllerViewModel {    
     var longForms = Binding<[LongForm]>(value: [LongForm]())
+    private var timer: Timer?
     
     func getLongForms(forAcronym acronym: String) {
-        AcronymParser.shared.getLongForms(forAcronym: acronym) {[weak self] longForms, error in
-            self?.longForms.value = longForms
+        timer?.invalidate()
+        
+        guard acronym.count > 1 else {
+            longForms.value = [LongForm]()
+            return
         }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+            AcronymParser.shared.getLongForms(forAcronym: acronym) {[weak self] longForms, error in
+                self?.longForms.value = longForms
+            }
+        }
+        
+        
+     
     }
     
     init() {
